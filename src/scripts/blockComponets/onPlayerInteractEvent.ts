694278706b476168b40e5edbfd64be8bfe268026
Pete9xi <peter95rose@msn.com>
2024-12-11 10:20:04 +0000

    [onPlayerInteractEvent] Add logic to test for locked block, with the idea of locking its configuration  and the possibility to trigger the configured data by hand. However i have not decided on how i want to process this as i may need to exclude the looping logic. For the time being we will just send the player a message to let them know this block is locked.

--- src/scripts/blockComponets/onPlayerInteractEvent.ts
+++ src/scripts/blockComponets/onPlayerInteractEvent.ts
@@ -1,4 +1,4 @@
-import { BlockComponentPlayerInteractEvent, world } from "@minecraft/server";
+import { BlockComponentPlayerInteractEvent, ItemStack, world } from "@minecraft/server";
 import { ModalFormData, ModalFormResponse } from "@minecraft/server-ui";
 export class recordBox {
     constructor() {
@@ -6,13 +6,18 @@ export class recordBox {
     }
     onPlayerInteract(e: BlockComponentPlayerInteractEvent) {
         const { player } = e;
+        //@ts-ignore seems to be an issue with the types EntityEquippableComponent
+        const playerequipComp: EntityEquippableComponent = player.getComponent("minecraft:equippable");
+        const mainHandItem: ItemStack = playerequipComp.getEquipment("Mainhand")
+        
+
         const blockLocationAsString = e.block.x.toString() + e.block.y.toString() + e.block.z.toString()
         //use the blocks location as a key.
         const testforDynamicProp = world.getDynamicProperty("cab" + blockLocationAsString)
         if (testforDynamicProp === undefined) {
             // Dynamic property doesn't exist so lets get the block setup and show the player a UI
             const configUI = new ModalFormData();
-            configUI.title("§rBlock Beats - Block Config");
+            configUI.title("§9Block Beats - Block Config");
             configUI.textField("FileName", "");
             configUI.textField("Track Length", "");
 			configUI.toggle("Loop?", false);
@@ -32,6 +37,25 @@ export class recordBox {
               return -1;
             });
         } else{
+            const testforLockDynamicProp = world.getDynamicProperty("cabLock" + blockLocationAsString);
+            //Check to see if the player is holding a stick if so we can then lock the block so it can be activated by hand.
+            if(mainHandItem.typeId =="minecraft:stick"){
+                if(testforLockDynamicProp === undefined){
+                    //we need to create it, this means the block should not be locked, lets lock it
+                    world.setDynamicProperty("cabLock" + blockLocationAsString,"locked");
+                    return;
+                }else{
+                    //The block should be locked so lets unlock it!
+                    world.setDynamicProperty("cabLock" + blockLocationAsString, undefined);
+                }
+
+            }
+            //Lets allow the player to activate the block by hand but for the time being we will advise on it being locked.
+            
+            if(testforLockDynamicProp === "locked"){
+                player.sendMessage(`§9Block Beats:§r This Block is Currently Locked. `);
+                return;
+            }
             //Lets allow the player to edit the current set data.
             const configUI = new ModalFormData();
 			const trackLengthProp = world.getDynamicProperty("cabLength" + blockLocationAsString);
@@ -39,7 +63,7 @@ export class recordBox {
 			if(trackLengthProp === undefined){
 			currentLoopingValue = false;
 			}
-            configUI.title("§rBlock Beats - Block Config");
+            configUI.title("§9Block Beats - Block Config");
             configUI.textField("FileName", testforDynamicProp.toString());
 			configUI.textField("Track Lenght", trackLengthProp.toString());
 			configUI.toggle("Loop?", currentLoopingValue); 

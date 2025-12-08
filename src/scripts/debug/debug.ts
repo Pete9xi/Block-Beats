c090070b0457d2f21ab782b4ddca492b1332a9a2
Pete9xi <peter95rose@msn.com>
2025-12-08 15:02:55 +0000

    define debug flags for certain events the current settings allow for basic debugging to be enabled i plan to add a command to disable and enable debugging in the game.

--- src/scripts/debug/debug.ts
+++ src/scripts/debug/debug.ts
@@ -1,1 +1,14 @@
-export const debugEnabled: boolean = false;
+export default {
+    /** Indicates whether debug mode is enabled. for certian events */
+    getKey: false,
+    setKey: false,
+    setBlockState: false,
+    getBlockState: false,
+    playSound: true,
+    stopSound: true,
+    tickKey: false,
+    currentBlockState: false,
+    saveConfig: true,
+    deleteBlockData: true,
+    registerBlocks: true,
+};

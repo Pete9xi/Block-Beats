fda1dce81b8015b06c11357c1a2f9e04b79f0265
Pete9xi <peter95rose@msn.com>
2026-05-08 21:48:24 +0100

    [feat] Custom Horns renaming a horn to the audio track name will play that back when used. Example record.mysoundbyte

--- src/scripts/custom-audio.ts
+++ src/scripts/custom-audio.ts
@@ -1,9 +1,11 @@
 import debug from "./debug/debug.js";
 import { registry } from "./event-listeners/registry.js";
 import { subscribeToWorldInitialize } from "./event-listeners/world-initialize.js";
+import { itemUseBeforeEvent } from "./item/item-use-event.js";
 
 // Conditionally log debug information before calling registry
 debug.debugMode && debug.registerBlocks && console.log("Block Beats [DEBUG]: Calling registry for blocks");
 
 registry();
 subscribeToWorldInitialize();
+itemUseBeforeEvent();
